5145ae141109689029a0628cbc313f022c0f3f00
NajimuddinS <sherlockshaikh@gmail.com>
2025-05-13 19:34:21 +0530

    files updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {HashRouter, BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import Anime from './pages/Anime'
 import Tshirts from './pages/Tshirts'
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/themes/anime" element={<Anime />} />
         <Route path="/tshirts" element={<Tshirts />} />
       </Routes>
-    </Router>
+    </HashRouter>
   )
 }
 
